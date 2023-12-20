b5f07dfb5a9d1322f4155d2640ffaff6b398cd1e
AmirHosein Ashkbar <129063168+AmirHosein-Ashkbar@users.noreply.github.com>
2023-12-20 18:23:18 +0330

    Building game cards

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,17 +3,16 @@ import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
 // Interfaces
-interface Game{
+export interface Game{
     id: number;
     name: string;
+    background_image: string; 
 }
 
 interface FetchGamesResponse{
     count: number;
     results: Game[];
 }
-
-
 
 const useGames = () =>{
     const [games , setGames] = useState<Game[]>([]);
